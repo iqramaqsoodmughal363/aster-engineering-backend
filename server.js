cea8eb15ab1748e9ec24e653e94d5ccf70d1fa35
Iqra Maqsood Mughal <iqramaqsoodmughal363@gmail.com>
2026-09-10 05:06:20 +0500

    Add vercel.json configuration and export app

--- server.js
+++ server.js
@@ -76,3 +76,4 @@ app.post('/api/login', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+module.exports = app;
